86c3253d6c074f5e05fcae207d483a47701f3d3c
yafsw <yafswctrlz@gmail.com>
2021-02-05 21:42:23 +0700

    Add App component & Styled Components library

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import React, { memo, StrictMode } from 'react';
+
+const App = () => {
+    return (
+        <StrictMode>
+            
+        </StrictMode>
+    );
+};
+
+export default memo(App);
