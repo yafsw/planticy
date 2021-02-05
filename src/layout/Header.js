6ca3424ddb0f2609aa918c495b5b918fcb7c5f23
yafsw <yafswctrlz@gmail.com>
2021-02-05 22:13:44 +0700

    Add Header Component

---
+++ src/layout/Header.js
@@ -0,0 +1,27 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+import NavBar from '../components/NavBar';
+
+const Header = () => {
+    return (
+        <HeaderStyle>
+            <NavBar />
+        </HeaderStyle>
+    );
+};
+
+const HeaderStyle = Styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 1.2rem;
+    position: sticky;
+    top: 0;
+    z-index: 99;
+    background-color: white;
+    box-shadow: 0 1.2rem 3rem rgba(0,175,145,.05);
+`;
+
+export default memo(Header);
