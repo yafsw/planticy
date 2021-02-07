3d6cb7ac4a202a7fb6c2666f0e04653d30bb9d3e
yafsw <yafswctrlz@gmail.com>
2021-02-07 15:06:25 +0700

    Add products data

---
+++ src/data/products.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+        id: 1,
+        url: 'https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=924&q=80',
+        name: 'Adele',
+        category: 'cactus',
+        price: 25,
+    },
+    {
+        id: 2,
+        url: 'https://images.unsplash.com/photo-1509222796416-4a1fef025e92?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=896&q=80',
+        name: 'Leighton',
+        category: 'cactus',
+        price: 12,
+    },
+    {
+        id: 3,
+        url: 'https://images.unsplash.com/photo-1508022713622-df2d8fb7b4cd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=675&q=80',
+        name: 'London',
+        category: 'cactus',
+        price: 19,
+    },
+    {
+        id: 4,
+        url: 'https://images.unsplash.com/photo-1509222413196-40eb6b6b96e2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=978&q=80',
+        name: 'Zella',
+        category: 'cacatus',
+        price: 27,
+    },
+    {
+        id: 5,
+        url: 'https://images.unsplash.com/photo-1509221625996-2f06de32cab8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=916&q=80',
+        name: 'Reese',
+        category: 'cactus',
+        price: 14,
+    },
+    {
+        id: 6,
+        url: 'https://images.unsplash.com/photo-1508022240297-e87369353901?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=675&q=80',
+        name: 'Brooklyn',
+        category: 'cactus',
+        price: 18,
+    },
+    {
+        id: 7,
+        url: 'https://images.unsplash.com/photo-1508021371391-516c9dd8fcfd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=938&q=80',
+        name: 'Cadence',
+        category: 'cactus',
+        price: 23,
+    },
+    {
+        id: 8,
+        url: 'https://images.unsplash.com/photo-1508023119239-ca43ffb5e861?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=884&q=80',
+        name: 'Troy',
+        category: 'cactus',
+        price: 29,
+    },
+    {
+        id: 9,
+        url: 'https://images.unsplash.com/photo-1509223197845-458d87318791?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=859&q=80',
+        name: 'Dori',
+        category: 'Succulent',
+        price: 15,
+    },
+    {
+        id: 10,
+        url: 'https://images.unsplash.com/photo-1508022463381-46d2f3d3f84a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80',
+        name: 'Spectra',
+        category: 'Succulent',
+        price: 24,
+    },
+    {
+        id: 11,
+        url: 'https://images.unsplash.com/photo-1511235873174-1888843f2ab0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80',
+        name: 'Margo',
+        category: 'Succulent',
+        price: 25,
+    },
+    {
+        id: 12,
+        url: 'https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=675&q=80',
+        name: 'Regy',
+        category: 'Succulent',
+        price: 10,
+    },
+];
+
+export default products;
