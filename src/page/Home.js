a65aa9fe2df97bc03e24836c46f530be3d6d58e1
yafsw <yafswctrlz@gmail.com>
2021-02-07 15:04:46 +0700

    Add MainHome component to Home component

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,12 @@
 import React, { memo } from 'react';
 import Header from '../layout/Header';
+import MainHome from '../layout/MainHome';
 
 const Home = () => {
     return (
         <>
             <Header />
+            <MainHome />
         </>
     );
 };
