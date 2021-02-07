5f5a6bd3e9f3f58176ebad4dad882fa3529b856a
yafsw <yafswctrlz@gmail.com>
2021-02-07 15:24:48 +0700

    Add additional setting & style to Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,26 @@
 import React, { memo } from 'react';
 import Styled from 'styled-components';
-import { secodary } from '../config/color';
+import { secondary } from '../config/color';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import banner from '../data/banner';
 
 const Banner = () => {
+    const settings = {
+        autoPlay: true,
+        disableButtonsControls: true,
+        infinite: true,
+        autoPlayInterval: 5000,
+        disableDotsControls: true,
+        autoHeight: true,
+        animationType: 'fadeout',
+        animationDuration: 1000,
+        mouseTracking: true
+    };
+
     return (
         <Container>
-            <AliceCarousel>
+            <AliceCarousel {...settings}>
                 {banner.map((item, index) => (
                     <BannerStyle key={index} src={item.url}>
                         <p>{item.quote}</p>
@@ -20,18 +32,19 @@ const Banner = () => {
 };
 
 const Container = Styled.section`
-    width: 100$;
+    width: 100%;
     max-width: 128rem;
     border-radius: 3rem;
     box-shadow: 0 1.2rem 3rem rgba(0,175,145,.03);
     margin-bottom: 1.2rem;
+    overflow: hidden;
 `;
 
 const BannerStyle = Styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: ${secodary};
+    background-color: ${secondary};
     background-image: url('${props => props.src}');
     background-image: linear-gradient(rgba(0, 121, 101, 0), rgba(0, 121, 101, .2)), url('${props => props.src}');
     background-position: center;
@@ -46,7 +59,7 @@ const BannerStyle = Styled.div`
     }
 
     p {
-        fonnt-size: 2.8rem;
+        font-size: 2.8rem;
         font-weight: bold;
         color: white;
     }
