3457aa8b6d4918d71d48c86af21b95dda22e4c51
yafsw <yafswctrlz@gmail.com>
2021-02-07 13:43:31 +0700

    Add banner data

---
+++ src/data/banner.js
@@ -0,0 +1,12 @@
+const banner = [
+    {
+        url: 'https://images.unsplash.com/photo-1583753075968-1236ccb83c66?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=691&q=80',
+        quote: 'The clearest way into the Universe is through a forest wilderness.'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1508013861974-9f6347163ebe?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1055&q=80',
+        quote: 'Trees and plants always look like the people they live with, somehow.'
+    },
+];
+
+export default banner;
