83f753bbe01f86ff847757849c6037f538594fe3
yafsw <yafswctrlz@gmail.com>
2021-02-07 15:56:20 +0700

    Add Product component

---
+++ src/components/Product.js
@@ -0,0 +1,49 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+import { secondary } from '../config/color';
+import Button from './Button';
+
+const Product = props => {
+    return (
+        <ProductStyle src={props.src}>
+            <span>{props.price}</span>
+            <Button primary >{props.name}</Button>
+        </ProductStyle>
+    );
+};
+
+const ProductStyle = Styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    height: 18rem;
+    width: 100%;
+    box-shadow: 0 1.2rem 3rem rgba(0,175,145,.03);
+    background-color: ${secondary};
+    background-image: url('${props => props.src}');
+    background-image: linear-gradient(rgba(0, 121, 101, 0), rgba(0, 121, 101, .2)), url('${props => props.src}');
+    background-size: cover;
+    background-position: center;
+    overflow: hidden;
+    padding: 1rem;
+    border-radius: 3rem;
+
+    &:hover {
+        background-image: linear-gradient(rgba(0, 121, 101, .1), rgba(0, 121, 101, .2)), url('${props => props.src}');
+    }
+
+    span {
+        font-size: 2.4rem;
+        padding: .5rem 1rem;
+        background-color: rgba(245,245,245,.8);
+        border-radius: 4rem;
+        transition: .3s ease-in-out;
+
+        &:hover {
+            background-color: white;
+        }
+    }
+`;
+
+export default memo(Product);
