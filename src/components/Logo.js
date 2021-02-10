a5e15e45869e4c98785baef06e0f2f2093fd5dcb
yafsw <yafswctrlz@gmail.com>
2021-02-10 19:36:34 +0700

    Update Logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,23 @@
 import React, { memo } from 'react';
 import Styled from 'styled-components';
 import { primary, tertiary } from '../config/color';
+import { Link } from 'react-router-dom';
 
 const Logo = props => {
     return (
-        <LogoStyle primary={props.primary}>
+        <LogoStyle primary={props.primary} to="/">
             Planticy
         </LogoStyle>
     );
 };
 
-const LogoStyle = Styled.span`
+const LogoStyle = Styled(Link)`
     font-size: 2.8rem;
     font-weight: bold;
     color: ${props => props.primary ? primary : 'white'};
     cursor: pointer;
     transition: .3s ease-in-out;
+    text-decoration: none;
 
     &:hover {
         color: ${tertiary};
