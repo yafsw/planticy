75ae28123e57664f7be6c4020642d8d501bee2ed
yafsw <yafswctrlz@gmail.com>
2021-02-11 10:32:14 +0700

    Update MainHome component

--- src/layout/MainHome.js
+++ src/layout/MainHome.js
@@ -2,23 +2,36 @@ import React, { memo } from 'react';
 import Styled from 'styled-components';
 import Banner from '../components/Banner';
 import Products from '../components/Products';
+import ProductsSearch from '../components/ProductsSearch';
+import { useApp } from '../context';
 
 const MainHome = () => {
-    return (
-        <MainHomeStyle>
-            <Banner />
-            <Products />
-        </MainHomeStyle>
-    );
+    const { state } = useApp();
+
+    if (state.search === '') {
+        return (
+            <MainHomeStyle>
+                <Banner />
+                <Products />
+            </MainHomeStyle>
+        );
+    } else {
+        return (
+            <MainHomeStyle>
+                <ProductsSearch />
+            </MainHomeStyle>
+        );
+    }
 };
 
 const MainHomeStyle = Styled.main`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     width: 100%;
     padding: 1.2rem;
+    min-height: 100vh;
     color: white;
 `;
 
