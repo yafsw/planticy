b85e14cf6157c2cf67055312ffb312f0c86f1594
yafsw <yafswctrlz@gmail.com>
2021-02-16 13:47:26 +0700

    Add ProductsBasketSearch component

---
+++ src/components/ProductsBasketSearch.js
@@ -0,0 +1,33 @@
+import React, { memo, useEffect, useState } from 'react';
+import Styled from 'styled-components';
+import ProductBasket from './ProductBasket';
+import { useApp } from '../context';
+
+const ProductsBasketSearch = () => {
+    const [productsState, setProductsState] = useState([]);
+    const { state } = useApp();
+
+    useEffect(() => {
+        const filter = state.basket.filter(item => item.name.toLowerCase().includes(state.search));
+        setProductsState(filter);
+    }, [state.search, state.basket]);
+
+    return (
+        <ProductsBasketSearchStyle>
+            {productsState.map(item => (
+                <ProductBasket src={item.url} name={item.name} price={item.price} key={item.id} />
+            ))}
+        </ProductsBasketSearchStyle>
+    );
+};
+
+const ProductsBasketSearchStyle = Styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 128rem;
+`;
+
+export default memo(ProductsBasketSearch);
